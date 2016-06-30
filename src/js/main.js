6fce86f358067d580d8c77212e761ad6e8b14c97
Rodney Fowlkes <Fowlkesrodney@gmail.com>
2016-06-30 18:27:30 -0400

    finished writing track request functionto git 200 tracks and sort through track information

---
+++ src/js/main.js
@@ -0,0 +1,57 @@
+import $ from 'Jquery';
+import _ from 'lodash';
+
+var client_id = "8762736d61261497aec348a0aff61e31" ;
+
+//var baseurl = "https://api.soundcloud.com/users/8482005?client_id=${client_id}";
+var baseurl = "https://api.soundcloud.com";
+
+var tracks;
+
+
+
+ 
+ var get_tracks = function(x, m){ $.ajax({ 
+	url: `${baseurl}/tracks`,
+	method: "GET",
+	dateType:"json",
+	data: {
+	  client_id: client_id,
+	  q: x,
+	  limit:200,
+	  linked_partitioning:3
+	}
+
+  }).then(function(obj) {
+    
+  	tracks = obj.collection;
+  	console.log(tracks);
+  	
+  	tracks.forEach(function(y) {
+    console.log(y.title);
+
+  	}); 
+  
+
+
+
+
+
+  });
+
+
+ ;}
+
+
+
+
+
+get_tracks()
+
+// $.ajax({
+// 	url: `baseurl/tracks`,
+// 	data: {
+// 		q: 'buskers',
+// 		license: 'cc-by-sa'
+// 	}
+// })
